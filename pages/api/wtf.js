b7cf436aedfa8b031d351b13e940ce7eb727dc5c
astatescu <angelostatescu.adobe@gmail.com>
2024-12-13 14:14:33 +0200

    test

--- pages/api/wtf.js
+++ pages/api/wtf.js
@@ -7,31 +7,31 @@ import { stringify } from "flatted";
 export default async function handler(req, res) {
   res.status(200).end();
 
-  // const slackResponse = {
-  //   blocks: [
-  //     {
-  //       type: "rich_text",
-  //       elements: [
-  //         {
-  //           type: "rich_text_section",
-  //           elements: [
-  //             {
-  //               type: "user",
-  //               user_id: req.body.text,
-  //             },
-  //             {
-  //               type: "text",
-  //               text: " was assigned today's shift!",
-  //               style: {
-  //                 italic: true,
-  //               },
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // };
+  const slackResponse = {
+    blocks: [
+      {
+        type: "rich_text",
+        elements: [
+          {
+            type: "rich_text_section",
+            elements: [
+              {
+                type: "text",
+                text: "I",
+              },
+              {
+                type: "text",
+                text: " was assigned today's shift!",
+                style: {
+                  italic: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
 
-  fetch(req.body.response_url, { method: "POST", body: stringify(req) });
+  fetch(req.body.response_url, { method: "POST", body: stringify(slackResponse) });
 }
